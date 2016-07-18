32df63e004d0ce24c56d8e65afb097b79dcade3d
junoooya <junoooya@gmail.com>
2016-07-18 17:11:41 +0900

    init login url and using jwt-token

--- airmap/airmap/urls.py
+++ airmap/airmap/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from rest_framework_jwt.views import obtain_jwt_token
+
 from users.api import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^api/signup/', SignupApi.as_view(), name='signup'),
-]
+    url(r'^api/login/', obtain_jwt_token),
+    ]
