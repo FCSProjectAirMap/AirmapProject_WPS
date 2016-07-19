9615707c21ba63053c50ad243ad8a95334e1154b
lioliolio <kpc00100@gmail.com>
2016-07-19 15:58:39 +0900

    add signup url

--- airmap/airmap/urls.py
+++ airmap/airmap/urls.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 from rest_framework_jwt.views import obtain_jwt_token
 
 from users.api import *
+from users.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^signup/', SignupView.as_view(),),
 
     url(r'^api/signup/', SignupApi.as_view(), name='signup'),
     url(r'^api/login/', obtain_jwt_token),
