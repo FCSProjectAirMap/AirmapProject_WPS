4cba3680ab91f3860c744615b9737cdc53052466
lioliolio <kpc00100@gmail.com>
2016-07-20 16:19:47 +0900

    fix typing error

--- airmap/airmap/urls/__init__.py
+++ airmap/airmap/urls/__init__.py
@@ -6,7 +6,7 @@ from users.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    
+
     url(r'^signup/', SignupView.as_view(), name="signup"),
     url(r'^login/', LoginView.as_view(), name="login"),
     url(r'^logout/', LogoutView.as_view(), name="logout"),
