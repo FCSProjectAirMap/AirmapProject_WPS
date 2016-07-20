bdb25aff7f3b0e8c3a093a6c9b555d72ac40967f
lioliolio <kpc00100@gmail.com>
2016-07-20 16:19:24 +0900

    add login, logout, signup urls

--- airmap/airmap/urls/__init__.py
+++ airmap/airmap/urls/__init__.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from users.views import *
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    
+    url(r'^signup/', SignupView.as_view(), name="signup"),
+    url(r'^login/', LoginView.as_view(), name="login"),
+    url(r'^logout/', LogoutView.as_view(), name="logout"),
 
     url(r'^api/', include("airmap.urls.api", namespace="api")),
     ]
