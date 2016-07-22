faa03ead36979bed4c568e9a096636e68a68cab4
lioliolio <kpc00100@gmail.com>
2016-07-23 01:53:42 +0900

    delete fuctions

--- airmap/travels/models/image_meta_data.py
+++ airmap/travels/models/image_meta_data.py
@@ -29,15 +29,3 @@ class TravelImageMetaData(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def get_local_time(self):
-        from travels.utils.timestamp import get_local_time\
-            as get_local_time_timestamp
-        return get_local_time_timestamp(self.latitude, self.longitude, self.timestamp)
-    local_time = property(get_local_time)
-
-    def get_location(self):
-        from travels.utils.location import get_location\
-            as get_location_data
-        return get_location_data(self.latitude, self.longitude)
-    location = property(get_location)
