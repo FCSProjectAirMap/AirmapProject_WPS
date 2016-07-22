b4ef96826bd2222c77556ca269158d5ae5d8fca7
lioliolio <kpc00100@gmail.com>
2016-07-23 03:00:02 +0900

    change file name: from image_meta_data to image_data, and add ImageField

---
+++ airmap/travels/models/image_data.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from users.models import User
+
+
+class TravelImageData(models.Model):
+
+    user = models.ForeignKey(User)
+    travel = models.ForeignKey("Travel")
+
+    latitude = models.CharField(
+        max_length=256,
+    )
+    longitude = models.CharField(
+        max_length=256,
+    )
+    country = models.CharField(
+        max_length=256,
+    )
+    city = models.CharField(
+        max_length=256,
+    )
+    timestamp = models.CharField(
+        max_length=256,
+    )
+
+    travel_image = models.ImageField(
+        blank=True,
+        null=True,
+    )
+
+    create_date = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
