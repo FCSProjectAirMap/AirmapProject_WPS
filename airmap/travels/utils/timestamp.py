6a847d36c08680cedbc17f208376abf760529254
lioliolio <kpc00100@gmail.com>
2016-07-22 15:44:50 +0900

    refactor get_local_time

--- airmap/travels/utils/timestamp.py
+++ airmap/travels/utils/timestamp.py
@@ -4,7 +4,7 @@ import json
 import requests
 
 
-def get_local_timestamp(latitude, longitude, timestamp):
+def get_local_time(latitude, longitude, timestamp):
 
     url = "https://maps.googleapis.com/maps/api/timezone/json?location={latitude},\
             {longitude}&timestamp={timestamp}&key={api_key}".format(
@@ -18,16 +18,12 @@ def get_local_timestamp(latitude, longitude, timestamp):
 
     timestamp_data = json.loads(response.text)
 
-    return timestamp_data
+    if timestamp_data.get("status") == "OK":
+        rawOffset = timestamp_data.get("rawOffset")
+        dstOffset = timestamp_data.get("dstOffset")
 
-
-def get_local_time(timestamp_dict_data, timestamp):
-
-    if timestamp_dict_data.get("status") == "OK":
-        rawOffset = timestamp_dict_data.get("rawOffset")
-        dstOffset = timestamp_dict_data.get("dstOffset")
-
-        result_timestamp = rawOffset + dstOffset + timestamp
+        result_timestamp = rawOffset + dstOffset + float(timestamp)
 
         result_date = datetime.datetime.fromtimestamp(result_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
         return result_date
