3b11f09fc2ced603044aa7bfddfad865d8ed94d6
lioliolio <kpc00100@gmail.com>
2016-07-24 15:30:33 +0900

    add TravelDataCreateAPIView url

--- airmap/airmap/urls/api.py
+++ airmap/airmap/urls/api.py
@@ -11,4 +11,5 @@ urlpatterns = [
         url(r'^login/', obtain_jwt_token),
         url(r'^travel/list/', TravelListAPIView.as_view(), name='travel_list'),
         url(r'^travel/detail/(?P<id>\d+)/', TravelDetailAPIView.as_view(), name='travel_detail'),
-        ]
+        url(r'^travel/create/', TravelDataCreateAPIView.as_view(), name='travel_create'),
+]
