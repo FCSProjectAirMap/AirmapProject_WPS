265dc12977f6dec72abcafe5a56714efd42dd2de
lioliolio <kpc00100@gmail.com>
2016-07-25 20:24:32 +0900

    add pipeline

--- airmap/airmap/settings/partials/base.py
+++ airmap/airmap/settings/partials/base.py
@@ -27,10 +27,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'pipeline',
+
     'users',
     'travels',
-
-    'rest_framework',
     ]
 
 MIDDLEWARE_CLASSES = [
