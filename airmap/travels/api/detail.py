7610654d7786b8c3099d24d0bbd849083c3c8d95
junoooya <junoooya@gmail.com>
2016-07-25 21:16:37 +0900

    refactor api/detail.py

--- airmap/travels/api/detail.py
+++ airmap/travels/api/detail.py
@@ -6,9 +6,6 @@ from travels.serializers import TravelDetailSerializer
 class TravelDetailAPIView(ListAPIView):
 
     def get_queryset(self):
-        self.request.user.active_travel = self.kwargs.get("id")
-        self.request.user.save()
         travel = self.request.user.travel_set.get(id=self.kwargs.get("id"))
-
         return travel.travelimagedata_set.all().order_by("created_date")
     serializer_class = TravelDetailSerializer
