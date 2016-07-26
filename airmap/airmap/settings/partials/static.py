d0453e51c59fd193eb743d0d151cf513ed2a01c1
lioliolio <kpc00100@gmail.com>
2016-07-26 17:20:45 +0900

    add pipeline settings

--- airmap/airmap/settings/partials/static.py
+++ airmap/airmap/settings/partials/static.py
@@ -13,3 +13,20 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT_DIR, "dist", "static")
 # Media files
 MEDIA_ROOT = os.path.join(PROJECT_ROOT_DIR, "dist", "images")
 MEDIA_URL = "/media/"
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'pipeline.finders.PipelineFinder',
+)
+
+PIPELINE = {
+    'STYLESHEETS': {
+        'airmap': {
+            'source_filenames': (
+              'css/application.css',
+            ),
+            'output_filename': 'css/airmap.css',
+        }
+    },
+}
